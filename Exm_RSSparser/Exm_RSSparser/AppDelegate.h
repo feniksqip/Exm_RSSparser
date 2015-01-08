972f35114b5c5ff44d48c218d85b7f93cac0faa0
Admin <admin@Mac-Admin.local>
2015-01-08 15:16:11 +0300

    Create...

---
+++ Exm_RSSparser/Exm_RSSparser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exm_RSSparser
+//
+//  Created by Admin on 08.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
